0e26f070eb684c3c8e018a58e67e61530d02abc4
신용준 <63823237+shinyj1991@users.noreply.github.com>
2022-11-09 09:44:05 +0900

    test 10

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,11 @@ import Link from 'next/link'
 
 type HelloData = {
   test: string
+  doc: any
 }
 
 async function getData(): Promise<HelloData> {
-  const res = await fetch(`${process.env.NEXT_API_ENTRY}/api/test`)
+  const res = await fetch(`${process.env.NEXT_API_ENTRY}/api/hello`)
   return res.json()
 }
 
@@ -16,7 +17,7 @@ export default async function Page() {
   return (
     <div>
       <div>
-        Test : {data.test}
+        Test : {data.doc.name}
         <br />
         <Link href="/about">about</Link>
       </div>
