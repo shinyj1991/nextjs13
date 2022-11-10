26ee1ce7e1ff61ee53b1c9a85a31940e1ce365a2
신용준 <63823237+shinyj1991@users.noreply.github.com>
2022-11-10 13:31:43 +0900

    test23

--- app/todo/page.jsx
+++ app/todo/page.jsx
@@ -1,23 +1,17 @@
 import Link from 'next/link'
 
-type TodoItem = {
-  message: string
-  success: boolean
-}
-
-async function getData(): Promise<TodoItem> {
+async function getData() {
   const res = await fetch(`${process.env.NEXT_API_ENTRY}/api/todo`)
   return res.json()
 }
 
 export default async function Page() {
-  const data = await getData()
-
-  console.log(data)
+  const { message } = await getData()
 
   return (
     <div>
       <h1>To do list</h1>
+      <p>{message}</p>
       <Link href="/">home</Link>
     </div>
   )
