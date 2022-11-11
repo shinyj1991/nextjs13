9433415e57f98ed544d81830f135aca38218cb57
신용준 <63823237+shinyj1991@users.noreply.github.com>
2022-11-11 12:02:53 +0900

    서버 코드 리팩토링2

--- pages/api/todo.ts
+++ pages/api/todo.ts
@@ -1,15 +1,13 @@
-import { connectToDatabase } from '../../lib/mongodb'
 import { server } from '../../lib/server'
 
 export default async function handler(req: any, res: any) {
   switch (req.method) {
     case 'GET': {
-      return server.get(req, res, async () => {
-        const { db } = await connectToDatabase()
-        const todos = await db.collection('todo').find({}).sort({ title: -1 }).toArray()
+      return server.get(req, res, async (database: any) => {
+        const todos = await database.collection('todo').find({}).sort({ title: -1 }).toArray()
 
         return {
-          data: JSON.parse(JSON.stringify(todos)),
+          data: todos,
           success: true,
         }
       })
